1a1085e6649acafa7d162e857de8909837fe0cb9
Alberto <alberto.renteriac@iteso.mx>
2024-08-05 20:05:44 -0600

    Update site URL and base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://betoc2.github.io/Portfolio/'
+  site: 'https://betoc2.github.io/Portfolio/',
+  base: '/Portfolio/',
 })
